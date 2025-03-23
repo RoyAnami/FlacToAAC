ed43e7c8dbb281309c9bbd51575a2a3ce898961e
Lychee <kyou_31@hotmail.com>
2025-03-24 00:46:36 +0900

    test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use std::process::Command;
+use std::path::Path;
+
+fn split_flac_with_cue(cue_path: &Path, flac_path: &Path) -> std::io::Result<()> {
+    Command::new("flac-tracksplit")
+        .arg(flac_path)
+        .arg(cue_path)
+        .status()?;
+    Ok(())
+}
+
+fn convert_flac_to_aac(flac_path: &Path, aac_path: &Path) -> std::io::Result<()> {
+    Command::new("qaac")
+        .arg("--no-optimize")
+        .arg("-V")
+        .arg("127")
+        .arg("-o")
+        .arg(aac_path)
+        .arg(flac_path)
+        .status()?;
+    Ok(())
 }
